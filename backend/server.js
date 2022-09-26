f44c4bb73f90e0d2e4584d2bd5b302d82420495f
Jason Thuma <jasonthuma@gmail.com>
2022-09-26 17:47:28 -0400

    modified server.js

--- backend/server.js
+++ backend/server.js
@@ -24,17 +24,14 @@ app.use("/api/", transactionRoutes);
 app.use("/api/", userRoutes);
 
 //Serve Frontend
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, "../", "frontend", "build", "index.html")
-    )
-  );
-} else {
-  app.get("/", (req, res) => res.send("Please set to production"));
-}
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", (req, res) =>
+  res.sendFile(
+    path.resolve(__dirname, "../", "frontend", "build", "index.html")
+  )
+);
 
 app.use(errorHandler);
 
